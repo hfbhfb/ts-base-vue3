f0606f0ea10751a0016040e111f1cea654415847
hefabao <hefabao@126.com>
2021-11-20 10:10:03 +0800

    ts是js的超集 , 是编译的时候检查的, 可以编译出js,但会报出错和警告

---
+++ 01.第一个ts应用/index.js
@@ -0,0 +1,5 @@
+function Helloworld(content) {
+    console.log(content);
+}
+// Helloworld(111) // 只是编译检查
+Helloworld("aaa");
---
+++ 01.第一个ts应用/index.ts
@@ -0,0 +1,10 @@
+
+function Helloworld(content:string){
+    console.log(content)
+}
+
+
+// Helloworld(111) // 只是编译检查
+Helloworld("aaa")
+
+
